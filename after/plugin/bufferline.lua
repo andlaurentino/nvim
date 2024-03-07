270fa713b10773b404e224dde5ea261ac40af124
Anderson Laurentino <anderson@laurentino.me>
2024-03-07 18:45:50 -0300

    fix: import the bufferline and mini correctly

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,4 +1,6 @@
+vim.opt.termguicolors = true
+
 local status_ok, bufferline = pcall(require, "bufferline")
-if not status_ok then
-  return
-end
+if not status_ok then return end
+
+bufferline.setup{}
--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -1,17 +1,17 @@
 
-local status_ok, treesitter = pcall(require, "mini.pairs")
+local status_ok, pairs = pcall(require, "mini.pairs")
 if not status_ok then return end
 
-local status_ok, treesitter = pcall(require, "mini.surround")
+local status_ok, surround = pcall(require, "mini.surround")
 if not status_ok then return end
 
-local status_ok, treesitter = pcall(require, "mini.trailspace")
+local status_ok, trailspace = pcall(require, "mini.trailspace")
 if not status_ok then return end
 
-local status_ok, treesitter = pcall(require, "mini.identscope")
+local status_ok, indentscope = pcall(require, "mini.indentscope")
 if not status_ok then return end
 
-local status_ok, treesitter = pcall(require, "comment")
+local status_ok, comment = pcall(require, "mini.comment")
 if not status_ok then return end
 
 
