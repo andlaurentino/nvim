17ffbd5a55b9b3c2f3a42b85aaab391d715eddea
Anderson Laurentino <anderson@laurentino.me>
2024-03-08 11:02:05 -0300

    feat: added the nvim-tree configuration

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,5 +1,72 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Open" )
+  vim.keymap.set("n", "l", api.node.open.edit, opts "Close Directory" )
+end
+
 return {
 	'nvim-tree/nvim-tree.lua',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {}
+	opts = {
+		on_attach = my_on_attach,
+		sort = {
+			sorter = "case_sensitive",
+		},
+		view = {
+			width = 30,
+		},
+		renderer = {
+			group_empty = true,
+			root_folder_modifier = ":t",
+			icons = {
+				glyphs = {
+					default = "",
+					symlink = "",
+					folder = {
+						arrow_open = "",
+						arrow_closed = "",
+						default = "",
+						open = "",
+						empty = "",
+						empty_open = "",
+						symlink = "",
+						symlink_open = "",
+					},
+					git = {
+						unstaged = "",
+						staged = "S",
+						unmerged = "",
+						renamed = "➜",
+						untracked = "U",
+						deleted = "",
+						ignored = "◌",
+					},
+				},
+			},
+		},
+		diagnostics = {
+			enable = true,
+			show_on_dirs = true,
+			icons = {
+				hint = "",
+				info = "",
+				warning = "",
+				error = "",
+			},
+		},
+		filters = {
+			dotfiles = false,
+			git_clean = false,
+			exclude = { ".git" }
+		},
+	}
 }
