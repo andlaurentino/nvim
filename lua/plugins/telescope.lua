ef12870bd4cec6075804fec43fdd294481df9ab6
Anderson Laurentino <anderson@laurentino.me>
2024-03-18 14:10:17 -0300

    feat: added description on the telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,12 +12,12 @@ return {
 		}
 		local builtin = require("telescope.builtin")
 
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc="Find files" })
+		vim.keymap.set('n', '<leader>p', builtin.find_files, { desc="Find files" })
+		vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc="Find git files" })
 		vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		vim.keymap.set('n', '<leader>ht', "<cmd>Telescope colorscheme<CR>", {})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc="Find opened buffers" })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc="Find helper" })
+		vim.keymap.set('n', '<leader>ht', "<cmd>Telescope colorscheme<CR>", { desc="Change color scheme" })
 	end
 }
