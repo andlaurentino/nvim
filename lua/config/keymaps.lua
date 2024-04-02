cd5082d638eac4ec6e29f037d44df490f22bb7bf
Anderson Laurentino <anderson@laurentino.me>
2024-04-02 12:30:55 -0300

    fix: close the Nvim Tree before close the buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,8 +5,8 @@ vim.cmd("xnoremap <expr> p 'pgv\"'.v:register.'y`>'")
 vim.cmd("xnoremap <noremap <expr> P 'Pgv\"'.v:register.'y`>'")
 
 -- Spliting
-vim.keymap.set("n", "|", vim.cmd.vsplit, { desc="Vertical split" })
-vim.keymap.set("n", "-", vim.cmd.split, { desc="Horizontal split" })
+vim.keymap.set("n", "|", vim.cmd.vsplit, { desc = "Vertical split" })
+vim.keymap.set("n", "-", vim.cmd.split, { desc = "Horizontal split" })
 
 -- Move one line on normal
 -- vim.keymap.set("n", "˚", function() vim.cmd.m("-2") end, { desc="Go next buffer" })
@@ -29,25 +29,29 @@ vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +5<CR>")
 vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -5<CR>")
 
 -- Navigate buffers / tabs
-vim.keymap.set("n", "<TAB>", "<cmd>bn!<CR>", { desc="Go next buffer" })
-vim.keymap.set("n", "<S-TAB>", "<cmd>bp!<CR>", { desc="Go previous buffer" })
+vim.keymap.set("n", "<TAB>", "<cmd>bn!<CR>", { desc = "Go next buffer" })
+vim.keymap.set("n", "<S-TAB>", "<cmd>bp!<CR>", { desc = "Go previous buffer" })
 
 -- Indent keeping the visual mode
-vim.keymap.set("v", "<tab>", ">gv", { desc="Indent forward" })
-vim.keymap.set("v", "<s-tab>", "<gv", { desc="Indent backwar" })
-vim.keymap.set("v", ">", ">gv", { desc="Indent forward" })
-vim.keymap.set("v", "<", "<gv", { desc="Indent backward" })
+vim.keymap.set("v", "<tab>", ">gv", { desc = "Indent forward" })
+vim.keymap.set("v", "<s-tab>", "<gv", { desc = "Indent backwar" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent forward" })
+vim.keymap.set("v", "<", "<gv", { desc = "Indent backward" })
 
 -- Close buffer / tab
-vim.keymap.set("n", "<C-w>", "<cmd>bd<CR>")
+-- vim.keymap.set("n", "<C-w>", "<cmd>bd<CR>")
+vim.keymap.set("n", "<C-w>", function()
+	vim.cmd('NvimTreeClose')
+	vim.cmd('bdelete')
+end)
 
 -- Save file
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc="Save buffer" })
-vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { desc="Save buffer" })
-vim.keymap.set("n", "<Leader>w", "<cmd>w<CR>", { desc="Save buffer" })
+vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save buffer" })
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", { desc = "Save buffer" })
+vim.keymap.set("n", "<Leader>w", "<cmd>w<CR>", { desc = "Save buffer" })
 
 -- Quit
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc="Quit" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
 
 -- Source file
 -- vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>")
