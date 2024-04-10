425ff2808d1bc8b606e8b66516128d13c78d0b0f
Anderson Laurentino <anderson@laurentino.me>
2024-04-10 00:18:27 -0300

    fix: configured correctly the treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,16 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "rust", "python", "go", "c", "lua", "vim", "vimdoc", "query", "javascript" },
+		ensure_installed = { "rust", "python", "go", "c", "lua", "vim", "query", "css", "javascript", "typescript", "svelte"  },
 
-		sync_install = false,
-
-		auto_install = true,
-
+		-- sync_install = false,
+		-- auto_install = true,
 		highlight = {
 			enable = true,
-			additional_vim_regex_highlighting = false,
+			-- additional_vim_regex_highlighting = false,
 		},
-	}
+	},
+	config = function (_, values)
+		require("nvim-treesitter.configs").setup(values)
+	end
 }
