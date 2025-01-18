ac95510c3dfb8769f876d105cd06cdb0d20ecc63
Anderson Laurentino <anderson@laurentino.me>
2025-01-18 16:27:12 -0300

    feat: updated the delay on the gitsign

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -8,7 +8,7 @@ return {
 				delete       = { text = '_' },
 				topdelete    = { text = '‾' },
 				changedelete = { text = '~' },
-				untracked    = { text = '┆' },
+				untracked    = { text = '│' },
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 			numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
@@ -23,13 +23,13 @@ return {
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-				delay = 1000,
+				delay = 100,
 				ignore_whitespace = false,
 				virt_text_priority = 100,
 			},
 			current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
 			sign_priority = 6,
-			update_debounce = 100,
+			update_debounce = 10,
 			status_formatter = nil, -- Use default
 			max_file_length = 40000, -- Disable if file is longer than this (in lines)
 			preview_config = {
